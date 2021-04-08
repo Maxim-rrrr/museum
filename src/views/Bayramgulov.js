31e85f566432ef141d19f842221ccfad2b3256d0
Maxim-rrrr <kewin.rrrr@gmail.com>
2021-04-09 02:13:27 +0300

    *: HeroesSection

--- src/views/Bayramgulov.js
+++ src/views/Bayramgulov.js
@@ -9,6 +9,8 @@ import arrow_down from "../img/arrow-down.svg";
 
 import { Link } from "react-router-dom";
 
+import HeroesSection from "./components/HeroesSection"
+
 const Bayramgulov = () => {
   return (
     <main className="heroes">
@@ -20,128 +22,94 @@ const Bayramgulov = () => {
         </Link>
       </LinkScroll>
 
-      <div className="heroes__section__bg">
-        <Element name="scr1">
-          <div className="container">
-            <section className="heroes__section">
-              <h1 className="heroes__name">
-                Байрамгу́лов Габдульма́н Хуснутди́нович
-              </h1>
+      <HeroesSection name="scr1" to="scr2">
+        <h1 className="heroes__name">
+          Байрамгу́лов Габдульма́н Хуснутди́нович
+        </h1>
 
-              <div className="heroes__section__content heroes__section__content--left">
-                <img src={photo_1} alt="" />
-                <p>
-                  Байрамгу́лов Габдульма́н Хуснутди́нович родился в 1906 году в
-                  деревне Иштуганово Юмагузинского района Республики Башкортостан.
-                  Окончил 4 класса деревенской школы, вступил в колхоз. В сентябре
-                  1941 года он получает повестку на службу в армии для защиты
-                  Родины от немецких захватчиков. В ноябре 1941 года в селе Алкино
-                  Габдульма́н Хуснутди́нович проходит подготовительные курсы
-                  молодого бойца. Затем в составе 112-ой Башкирской кавалерийской
-                  дивизии под командованием генерала Шаймуратова принимает участие
-                  в обороне Москвы.
-                </p>
-              </div>
+        <div className="heroes__section__content heroes__section__content--left">
+          <img src={photo_1} alt="" />
+          <p>
+            Байрамгу́лов Габдульма́н Хуснутди́нович родился в 1906 году в
+            деревне Иштуганово Юмагузинского района Республики Башкортостан.
+            Окончил 4 класса деревенской школы, вступил в колхоз. В сентябре
+            1941 года он получает повестку на службу в армии для защиты
+            Родины от немецких захватчиков. В ноябре 1941 года в селе Алкино
+            Габдульма́н Хуснутди́нович проходит подготовительные курсы
+            молодого бойца. Затем в составе 112-ой Башкирской кавалерийской
+            дивизии под командованием генерала Шаймуратова принимает участие
+            в обороне Москвы.
+          </p>
+        </div>
 
-              <LinkScroll
-                activeClass="active"
-                to="scr2"
-                spy={true}
-                smooth={true}
-                duration={500}
-              >
-                <div className="heroes__arrow-down-box">
-                  <img src={arrow_down} alt="" className="heroes__arrow-down" />
-                </div>
-              </LinkScroll>
-            </section>
+      </HeroesSection>
+
+      <HeroesSection name="scr2" to="scr3">
+        <div className="heroes__section__content heroes__section__content--right">
+          <img src={photo_2} alt="" />
+          <p>
+            [Минигали́ Минга́зович Шаймура́тов — советский военачальник,
+            командир 112-й Башкирской кавалерийской дивизии во время Великой
+            Отечественной войны. Генерал-майор. Герой Российской Федерации.]
+            В марте 1943 года в штаб дивизии приходят документы на
+            Габдульмана Хуснутдиновича о извещением о том, что он является
+            сыном раскулаченного кулака. Служба НКВД направляют его для
+            далнейшего прохождения службы в штрафной батальон в район города
+            Калач Сталинградской области в подразделения под командованиеем
+            маршала Константина Константиновича Рокоссовского. С первых дней
+            в штрафном батальоне он участвует в обороне города - героя
+            Сталинграда. В жестоких схватках с немецкими оккупантами мой
+            прадед получает несколько ранений.
+          </p>
+        </div>
+
+        <LinkScroll
+          activeClass="active"
+          to="scr3"
+          spy={true}
+          smooth={true}
+          duration={500}
+        >
+          <div className="heroes__arrow-down-box">
+            <img src={arrow_down} alt="" className="heroes__arrow-down" />
           </div>
-        </Element>
-
-      </div>
-
-
-      <div className="heroes__section__bg">
-        <Element name="scr2">
-          <div className="container">
-            <section className="heroes__section">
-              <div className="heroes__section__content heroes__section__content--right">
-                <img src={photo_2} alt="" />
-                <p>
-                  [Минигали́ Минга́зович Шаймура́тов — советский военачальник,
-                  командир 112-й Башкирской кавалерийской дивизии во время Великой
-                  Отечественной войны. Генерал-майор. Герой Российской Федерации.]
-                  В марте 1943 года в штаб дивизии приходят документы на
-                  Габдульмана Хуснутдиновича о извещением о том, что он является
-                  сыном раскулаченного кулака. Служба НКВД направляют его для
-                  далнейшего прохождения службы в штрафной батальон в район города
-                  Калач Сталинградской области в подразделения под командованиеем
-                  маршала Константина Константиновича Рокоссовского. С первых дней
-                  в штрафном батальоне он участвует в обороне города - героя
-                  Сталинграда. В жестоких схватках с немецкими оккупантами мой
-                  прадед получает несколько ранений.
-                </p>
-              </div>
-
-              <LinkScroll
-                activeClass="active"
-                to="scr3"
-                spy={true}
-                smooth={true}
-                duration={500}
-              >
-                <div className="heroes__arrow-down-box">
-                  <img src={arrow_down} alt="" className="heroes__arrow-down" />
-                </div>
-              </LinkScroll>
-            </section>
-          </div>
-        </Element>
-      </div>
+        </LinkScroll>
+      </HeroesSection>
 
       
-      <div className="heroes__section__bg">
-        <Element name="scr3">
-          <div className="container">
-            <section className="heroes__section">
-              <div className="heroes__section__content heroes__section__content--left">
-                <img src={photo_3} alt="" />
-                <p>
-                  [Константи́н Константи́нович Рокоссо́вский— советский и польский
-                  военачальник, дважды Герой Советского Союза.] В январе 1943 года
-                  он принимает участие в разгроме 6 полевой армии врага и взятия в
-                  плен фельдмаршала Фридриха Паулюса. За боевые заслуги из
-                  штрафного батальона был переведен в роту охраны Константина
-                  Рокоссовского. Далее в составе армии Рокоссовского участвует во
-                  многих сражениях по разгрому фашистов. День Победы он встретил в
-                  Германии на реке Эльба совместно с американскими войсками.
-                  Габдульман Хуснутдинович воевал в подразделении, под
-                  непосредственным командованием маршала Рокоссовского, то есть
-                  боевой путь маршала Рокоссовского – это путь Габдульмана
-                </p>
-              </div>
+      <HeroesSection name="scr3" to="scr4">
+        <div className="heroes__section__content heroes__section__content--left">
+          <img src={photo_3} alt="" />
+          <p>
+            [Константи́н Константи́нович Рокоссо́вский— советский и польский
+            военачальник, дважды Герой Советского Союза.] В январе 1943 года
+            он принимает участие в разгроме 6 полевой армии врага и взятия в
+            плен фельдмаршала Фридриха Паулюса. За боевые заслуги из
+            штрафного батальона был переведен в роту охраны Константина
+            Рокоссовского. Далее в составе армии Рокоссовского участвует во
+            многих сражениях по разгрому фашистов. День Победы он встретил в
+            Германии на реке Эльба совместно с американскими войсками.
+            Габдульман Хуснутдинович воевал в подразделении, под
+            непосредственным командованием маршала Рокоссовского, то есть
+            боевой путь маршала Рокоссовского – это путь Габдульмана
+          </p>
+        </div>
 
-              <LinkScroll
-                activeClass="active"
-                to="scr4"
-                spy={true}
-                smooth={true}
-                duration={500}
-              >
-                <div className="heroes__arrow-down-box">
-                  <img src={arrow_down} alt="" className="heroes__arrow-down" />
-                </div>
-              </LinkScroll>
-            </section>
+        <LinkScroll
+          activeClass="active"
+          to="scr4"
+          spy={true}
+          smooth={true}
+          duration={500}
+        >
+          <div className="heroes__arrow-down-box">
+            <img src={arrow_down} alt="" className="heroes__arrow-down" />
           </div>
-        </Element>
-      </div>
+        </LinkScroll>
+      </HeroesSection>
 
 
-      <div className="heroes__section__bg">             
-        <Element name="scr4">
-          <div className="container">
-            <section className="heroes__section">
+      <HeroesSection name="scr4">
               <h2 className="medals__name">
                 Габдульман Хуснутдинович <br />
                 <span>был награжден медалями:</span>
@@ -165,10 +133,7 @@ const Bayramgulov = () => {
                 Габдульман Хуснутдинович, ветеран войны, ветеран труда,
                 заслуженный работник Урала, умер в августе 1982 года.
               </div>
-            </section>
-          </div>
-        </Element>
-      </div>
+            </HeroesSection>
     </main>
   );
 };
--- src/views/components/HeroesSection.js
+++ src/views/components/HeroesSection.js
@@ -1,5 +1,33 @@
+import { Link as LinkScroll, Element } from "react-scroll";
+import arrow_down from "../../img/arrow-down.svg";
+
 const HeroesSection = (props) => {
   return (
-    
-  )
-}
+    <div className="heroes__section__bg">
+      <Element name={ props.name }>
+        <div className="container">
+          <section className="heroes__section">
+            { props.children }
+
+            {
+              props.to && 
+              <LinkScroll
+                activeClass="active"
+                to={ props.to }
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                <div className="heroes__arrow-down-box">
+                  <img src={arrow_down} alt="" className="heroes__arrow-down" />
+                </div>
+              </LinkScroll>
+            }
+          </section>
+        </div>
+      </Element>
+    </div>
+  );
+};
+
+export default HeroesSection
