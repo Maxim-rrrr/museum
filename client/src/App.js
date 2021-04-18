e9f5b441234cd543ac0b07a43d5fa5a36bb85b05
Maxim-rrrr <kewin.rrrr@gmail.com>
2021-04-18 12:17:44 +0300

    *: removed unnecessary

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,11 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./views/Main/index"
 import AdminRouter from "./views/admin/router"
 import AddHero from "./views/addHero"
-import Bayramgulov from "./views/Bayramgulov"
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/heroes/0" component={ Bayramgulov } />
-
         <Route exact path="/admin" component={ AdminRouter } />
         <Route exact path="/admin/*" component={ AdminRouter } />
 
--- client/src/views/components/HeroesTable.js
+++ client/src/views/components/HeroesTable.js
@@ -1,59 +1,6 @@
-import photo_1 from "../../img/photos/Байрамгулов.png"
-import photo_2 from "../../img/photos/Акиров.png"
-import photo_3 from "../../img/photos/Гирфанов.png"
-import photo_4 from "../../img/photos/Сафин.png"
-import photo_5 from "../../img/photos/Шунулина.png"
-import photo_6 from "../../img/photos/Талипов.png"
-import photo_7 from "../../img/photos/Тареев.png"
-import photo_8 from "../../img/photos/Кочуров.png"
-import photo_9 from "../../img/photos/Сагдиев.png"
-import photo_10 from "../../img/photos/Аглямов.png"
-
 import { Link as LinkScroll } from "react-scroll";
 import { Link } from 'react-router-dom'
 
-const cards = [
-  {
-    photo: photo_1,
-    name: "Байрамгулов Г. Х."
-  },
-  {
-    photo: photo_2,
-    name: "Акиров Р. А."
-  },
-  {
-    photo: photo_3,
-    name: "Гирфанов Ф. Б."
-  },
-  {
-    photo: photo_4,
-    name: "Сафин К. С."
-  },
-  {
-    photo: photo_5,
-    name: "Шунулина М. П."
-  },
-  {
-    photo: photo_6,
-    name: "Талипов Н. Т."
-  },
-  {
-    photo: photo_7,
-    name: "Тареев С. Н."
-  },
-  {
-    photo: photo_8,
-    name: "Кочуров И. В."
-  },
-  {
-    photo: photo_9,
-    name: "Сагдиев С. Ш."
-  },
-  {
-    photo: photo_10,
-    name: "Алгямов З. А."
-  }
-]
 
 const HeroesTable = () => {
   return (
@@ -61,7 +8,7 @@ const HeroesTable = () => {
       <h2 className="photos__title"> Наши герои </h2>
 
       <div className="cards">
-        {
+        {/* {
           cards.map((card, index) => {
             return (
               <LinkScroll
@@ -77,7 +24,7 @@ const HeroesTable = () => {
               </LinkScroll>
             )
           })
-        }
+        } */}
       </div>
     </section>
   )
