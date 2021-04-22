30600ee3aa4a0647d65d32afda2d05a1761661c4
Maxim-rrrr <kewin.rrrr@gmail.com>
2021-04-22 17:24:01 +0300

    *: heroes table

--- client/src/views/components/HeroesTable.js
+++ client/src/views/components/HeroesTable.js
@@ -100,12 +100,19 @@ const HeroesTable = (props) => {
           {
             cards.map((card) => {
               return (
-                <Link to={"/hero/" + card.id} className="card">
-                  <div className="photos__img">
-                    <img src= { `/uploads/${card.photo}` } alt=""/>
-                  </div>
-                  <strong> { card.name } </strong>
-                </Link>
+                <LinkScroll
+                  activeClass="active"
+                  to="scr1"
+                  spy={true}
+                  offset={0}
+                >
+                  <Link to={"/hero/" + card.id} className="card">
+                    <div className="photos__img">
+                      <img src= { `/uploads/${card.photo}` } alt=""/>
+                    </div>
+                    <strong> { card.name } </strong>
+                  </Link>
+                </LinkScroll>
               )
             })
           }
